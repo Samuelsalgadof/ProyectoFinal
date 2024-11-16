a8feb13feb138c61d9c4936ad1f8ac6fc02fc393
Sanml222 <Santiagoml222@gmail.com>
2024-11-16 02:02:13 -0500

    View controller cuenta y presupuesto

--- co.edu.unquindio.proyectofinal/src/main/java/co/edu/unquindio/proyectofinal/dwr/viewController/PresupuestoViewController.java
+++ co.edu.unquindio.proyectofinal/src/main/java/co/edu/unquindio/proyectofinal/dwr/viewController/PresupuestoViewController.java
@@ -1,4 +1,68 @@
 package co.edu.unquindio.proyectofinal.dwr.viewController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class PresupuestoViewController {
+
+    @FXML
+    private Button btnActualizarPresupuesto;
+
+    @FXML
+    private Button btnAgregarPresupuesto;
+
+    @FXML
+    private Button btnEliminarPresupuesto;
+
+    @FXML
+    private Button btnNuevoPresupuesto;
+
+    @FXML
+    private TextField intMontoAsignado;
+
+    @FXML
+    private TableView<?> tablaUsuario;
+
+    @FXML
+    private TableColumn<?, ?> tcMontoGastado;
+
+    @FXML
+    private TableColumn<?, ?> tcidPresupuesto;
+
+    @FXML
+    private TableColumn<?, ?> tcmontoAsignado;
+
+    @FXML
+    private TableColumn<?, ?> tcnombrePresupuesto;
+
+    @FXML
+    private TextField txtNombrePresupuesto;
+
+    @FXML
+    private TextField txtidPresupuesto;
+
+    @FXML
+    void onActualizarPresupuesto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAgregarPresupuesto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEliminarPresupuesto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onNuevoPresupuesto(ActionEvent event) {
+
+    }
+
 }
